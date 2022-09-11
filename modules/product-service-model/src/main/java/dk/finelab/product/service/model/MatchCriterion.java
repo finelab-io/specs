1b948131ee0938ca76223837ee7bb44f440c498d
Michael Kofoed <miko@imac.local>
2022-09-11 15:19:18 +0200

    refactor

---
+++ modules/product-service-model/src/main/java/dk/finelab/product/service/model/MatchCriterion.java
@@ -0,0 +1,47 @@
+package dk.finelab.product.service.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MatchCriterion {
+
+	public MatchCriterion () {
+	}
+
+	public MatchCriterion (
+		String name, 
+		String value) {
+		this.name = name;
+		this.value = value;
+	}
+
+	private String name;
+	private String value;
+	public String getName() {
+		return name;
+	}
+
+	public MatchCriterion setName(String name) {
+		this.name = name;
+		return this;
+	}
+
+
+	public String getValue() {
+		return value;
+	}
+
+	public MatchCriterion setValue(String value) {
+		this.value = value;
+		return this;
+	}
+
+	public String toString() {
+		return "MatchCriterion ["
+		+ " name: " + name
+		+ " value: " + value
+		+ " ]";
+	}
+}
